9180959005dc125f77e7926470fe33d954447bf2
eloytf <eloytf@gmail.com>
2016-01-27 23:37:10 +0100

    parsed main files: set, labels (x2) and mapping.

---
+++ run_analysis.R
@@ -0,0 +1,10 @@
+xtrain<-read.table("./UCI HAR Dataset/train/X_train.txt",sep="")
+ytrain<-read.table("./UCI HAR Dataset/train/Y_train.txt",sep="")
+xtest<-read.table("./UCI HAR Dataset/test/X_test.txt",sep="")
+ytest<-read.table("./UCI HAR Dataset/test/Y_test.txt",sep="")
+
+
+subtest<-read.table("./UCI HAR Dataset/test/subject_test.txt",sep="")
+subtrain<-read.table("./UCI HAR Dataset/train/subject_train.txt",sep="")
+
+levmap<-read.table("./UCI HAR Dataset/activity_labels.txt")
